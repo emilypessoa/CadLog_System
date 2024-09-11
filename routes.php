489827f420bd6d3e8bb4155db5b9601b0bc0b9df
Emily <emymi2007@gmail.com>
2024-09-11 10:15:06 -0300

    Inclui arquivos de controller, cria instâncias e coleta ação URL

--- routes.php
+++ routes.php
@@ -1,10 +1,29 @@
 <?php
 
-// Inclui arquivos de controlador ára lidar com diferentes ações
-require 'controllers/AuthController.php'
+// Inclui arquivos de controlador para lidar com diferentes ações
+
+require 'controllers/AuthController.php';
 // Inclui o controlador de autenticação
-require 'controllers/UserController.php'
+require 'controllers/UserController.php';
 // Inclui o controlador de usuário
-require 'controller/DashboardController.php'
+require 'controller/DashboardController.php';
 // Inclui o controlador de dashboard
+
+// Cria instâncias dos controladores para utilizar seus métodos
+
+$authController = new AuthController(); 
+// Instância o controlador de autenticação
+
+// Coleta a ação da URL, se não houver definida, usa 'login' por padrão
+
+$action = $_GET['action'] ?? 'login';
+// Usa operador de coalescência nula (??) para definir 'login' se 'action' não estiver presente
+
+// Verifica a ação solicitada e chama o método apropriado do controlador
+
+switch($action){
+    case 'login':
+    $authController->login();
+    // Chama o método login do controlador de autenticação
+}
 ?>
