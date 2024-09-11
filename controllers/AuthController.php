54c92c895946bbbcc2c6907e90d33ff648e91727
Emily <emymi2007@gmail.com>
2024-09-11 10:17:26 -0300

    Criação da função login e pagina login

---
+++ controllers/AuthController.php
@@ -0,0 +1,14 @@
+<?php
+
+// Requer arquivo 'user.php' que contem o model user com as funções para manipulação de dados de usário
+require_once 'models/user.php';
+
+class AuthController
+{
+    // Cria função responsável pelo processo de login
+    public function login()
+    {
+        
+    }
+}
+?>
