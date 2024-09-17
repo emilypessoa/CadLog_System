b8d1fbed24e50a289a1b317fe71b4d3a9e3066f2
Emily <emymi2007@gmail.com>
2024-09-17 12:20:03 -0300

    Instância o controlador de User

--- routes.php
+++ routes.php
@@ -1,26 +1,18 @@
 <?php
 
 // Inclui arquivos de controlador para lidar com diferentes ações
-
-require 'controllers/AuthController.php';
-// Inclui o controlador de autenticação
-require 'controllers/UserController.php';
-// Inclui o controlador de usuário
-require 'controller/DashboardController.php';
-// Inclui o controlador de dashboard
+require 'controllers/AuthController.php'; // Inclui o controlador de autenticação
+require 'controllers/UserController.php'; // Inclui o controlador de usuário
+require 'controller/DashboardController.php'; // Inclui o controlador de dashboard
 
 // Cria instâncias dos controladores para utilizar seus métodos
-
-$authController = new AuthController(); 
-// Instância o controlador de autenticação
+$authController = new AuthController(); // Instância o controlador de autenticação
+$userController = new UserController();
 
 // Coleta a ação da URL, se não houver definida, usa 'login' por padrão
-
-$action = $_GET['action'] ?? 'login';
-// Usa operador de coalescência nula (??) para definir 'login' se 'action' não estiver presente
+$action = $_GET['action'] ?? 'login'; // Usa operador de coalescência nula (??) para definir 'login' se 'action' não estiver presente
 
 // Verifica a ação solicitada e chama o método apropriado do controlador
-
 switch($action){
     case 'login':
     $authController->login();
